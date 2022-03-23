aef9a087f196711a08e169cfeace65bb498c1066
balazstimea <balazstimea9212@gmail.com>
2022-03-23 15:41:00 +0200

    new exercise

--- src/udemy/interfaces/abstractClasses/binarySearchTree/Main.java
+++ src/udemy/interfaces/abstractClasses/binarySearchTree/Main.java
@@ -3,14 +3,48 @@ package udemy.interfaces.abstractClasses.binarySearchTree;
 public class Main {
         public static void main(String[] args){
 
-        MyLinkedList list = new MyLinkedList(null);
+        /*MyLinkedList list = new MyLinkedList(null);
 
-        String stingData="Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        //String stingData="Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+
+        String stingData="5 7 3 9 8 2 1 0 4 6";
 
         String[] data =stingData.split(" ");
         for (String s: data){
             list.addItem(new Node(s));
         }
         list.traverse(list.getRoot());
+        list.removeItem(new Node("3"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("5"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("0"));
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node("2"));
+        list.traverse(list.getRoot());*/
+
+                SearchTree tree = new SearchTree(null);
+
+                //String stingData="Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+
+                String stingData="5 7 3 9 8 2 1 0 4 6";
+
+                String[] data =stingData.split(" ");
+                for (String s: data){
+                        tree.addItem(new Node(s));
+                }
+                tree.traverse(tree.getRoot());
+                tree.removeItem(new Node("3"));
+                tree.traverse(tree.getRoot());
+
+                tree.removeItem(new Node("5"));
+                tree.traverse(tree.getRoot());
+
+                tree.removeItem(new Node("0"));
+                tree.removeItem(new Node("4"));
+                tree.removeItem(new Node("2"));
+                tree.traverse(tree.getRoot());
     }
 }
--- src/udemy/interfaces/abstractClasses/binarySearchTree/MyLinkedList.java
+++ src/udemy/interfaces/abstractClasses/binarySearchTree/MyLinkedList.java
@@ -47,6 +47,28 @@ public class MyLinkedList implements NodeList{
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item !=null){
+            System.out.println("Deleting item "+ item.getValue());
+        }
+        ListItem currentItem=this.root;
+        while(currentItem != null){
+            int comparison = currentItem.compareTo(item);
+            if (comparison ==0){
+                //found the item to delete
+                if (currentItem == this.root){
+                    this.root = currentItem.next();
+                } else{
+                    currentItem.previous().setNext(currentItem.next());
+                    if (currentItem.next() != null){
+                        currentItem.next().setPrevious(currentItem.previous());
+                    }
+                }
+                return true;
+            } else if (comparison<0){
+                currentItem= currentItem.next();
+            } else //comparison>0
+            return false;
+        }
         return false;
     }
 
